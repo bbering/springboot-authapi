fb404ae8fe8b51720e2662b411af6536ec155f9a
Breno Bering <bbering88@gmail.com>
2024-10-04 14:20:35 -0300

    springboot + dependencias

---
+++ src/main/java/com/bbering/crm/bbering_api/BberingApiApplication.java
@@ -0,0 +1,13 @@
+package com.bbering.crm.bbering_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BberingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BberingApiApplication.class, args);
+	}
+
+}
