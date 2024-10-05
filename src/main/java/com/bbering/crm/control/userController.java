02a0fb1f69910b21a6c52e4b318d4e9715dd91f7
Breno Bering <bbering88@gmail.com>
2024-10-05 15:34:13 -0300

    putmapping

--- src/main/java/com/bbering/crm/control/userController.java
+++ src/main/java/com/bbering/crm/control/userController.java
@@ -3,13 +3,13 @@ package com.bbering.crm.control;
 import java.util.List;
 import java.util.Optional;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -46,4 +46,21 @@ public class UserController {
       return ResponseEntity.notFound().build(); // retorna 404 not found se nao encontrar o usuario
     }
   }
+
+  @PutMapping
+  public ResponseEntity<User> updateUser(@RequestBody User userDetails) {
+    Optional<User> userToUpdate = userRepository.findById(userDetails.getId());
+    if (userToUpdate.isPresent()) {
+      User user = userToUpdate.get();
+
+      user.setName(userDetails.getName());
+      user.setId(userDetails.getId());
+
+      User updatedUser = userRepository.save(user);
+      return ResponseEntity.ok(updatedUser);
+    } else {
+      return ResponseEntity.notFound().build(); // erro 404, nao encontrado
+    }
+  }
+
 }
