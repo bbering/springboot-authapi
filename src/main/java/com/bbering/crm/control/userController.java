5e1db08f8bba8e3ef9e5fd0000bfceba81b95f1f
Breno Bering <bbering88@gmail.com>
2024-10-05 15:19:53 -0300

    adicionado postmapping

--- src/main/java/com/bbering/crm/control/userController.java
+++ src/main/java/com/bbering/crm/control/userController.java
@@ -1,9 +1,13 @@
 package com.bbering.crm.control;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -30,5 +34,16 @@ public class UserController {
   @ResponseStatus(HttpStatus.CREATED)
   public User putUser(@RequestBody User user) {
     return userRepository.save(user);
+  }
+
+  @DeleteMapping
+  public ResponseEntity<Void> removeUser(@RequestBody User user) {
+    Optional<User> userToDelete = userRepository.findById(user.getId());
+    if (userToDelete.isPresent()) {
+      userRepository.delete(userToDelete.get());
+      return ResponseEntity.noContent().build(); // retorna 204 no Content se for possivel deletar
+    } else {
+      return ResponseEntity.notFound().build(); // retorna 404 not found se nao encontrar o usuario
+    }
   }
 }
