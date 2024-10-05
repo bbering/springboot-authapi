9f38678d96805bad21b0c16be4b79eb66c7a333e
Breno Bering <bbering88@gmail.com>
2024-10-05 15:13:19 -0300

    create status 201

--- src/main/java/com/bbering/crm/control/userController.java
+++ src/main/java/com/bbering/crm/control/userController.java
@@ -3,10 +3,12 @@ package com.bbering.crm.control;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bbering.crm.model.User;
@@ -25,6 +27,7 @@ public class UserController {
   }
 
   @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
   public User putUser(@RequestBody User user) {
     return userRepository.save(user);
   }
