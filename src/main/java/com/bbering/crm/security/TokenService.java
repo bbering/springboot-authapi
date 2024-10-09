5b7abd62b6f8bd15ea80d1e21c226ad7b4f85bc2
Breno Bering <bbering88@gmail.com>
2024-10-08 23:10:23 -0300

    + jwt token service class

---
+++ src/main/java/com/bbering/crm/security/TokenService.java
@@ -0,0 +1,38 @@
+package com.bbering.crm.security;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.bbering.crm.model.User;
+
+@Service
+public class TokenService {
+
+  @Value("${api.security.token.secret}")
+  private String secret;
+
+  public String generateToken(User user) {
+    try {
+      Algorithm algorithm = Algorithm.HMAC256(secret);
+      String token = JWT.create()
+          .withIssuer("auth-api")
+          .withSubject(user.getLogin())
+          .withExpiresAt(generateExpDate())
+          .sign(algorithm);
+      return token;
+    } catch (JWTCreationException exception) {
+      throw new RuntimeException("Error while generating token", exception);
+    }
+  }
+
+  private Instant generateExpDate() {
+    return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+  }
+}
