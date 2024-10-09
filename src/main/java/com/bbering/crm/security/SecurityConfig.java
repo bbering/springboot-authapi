a300a3415759b6deddcab6b442a458b0e4029160
Breno Bering <bbering88@gmail.com>
2024-10-08 21:01:56 -0300

    adicionada regra para requisicao post, necessario ter role admin, as demais liberadas

--- src/main/java/com/bbering/crm/security/SecurityConfig.java
+++ src/main/java/com/bbering/crm/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package com.bbering.crm.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -10,10 +12,14 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+  @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
     return httpSecurity
         .csrf(csrf -> csrf.disable())
         .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authorizeHttpRequests(authorize -> authorize
+        .requestMatchers(HttpMethod.POST, "/users").hasRole("ADMIN")
+        .anyRequest().authenticated())
         .build();
   }
 
